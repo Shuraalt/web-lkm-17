ed3cb4d6381e2f1ad38afab17c0fef68f564c32a
Nadila <nadilanuryadina@gmail.com>
2026-05-14 15:19:33 +0700

    isi materi dari nabilla

--- src/lib/materi.ts
+++ src/lib/materi.ts
@@ -72,7 +72,25 @@ Proses memahami kondisi masyarakat secara menyeluruh untuk merancang dan melaksa
       pemateri: "A Arul Mishabul Khoer", 
       tanggal: "Sabtu, 25 April 2026", 
       iconType: "ExternalLink",
-      isi: `
+      isi: ` 
+      
+      Dinamika Sosial dan Politik
+
+      Ideologi Kiri & Kanan
+      Muncul saat reformasi prancis
+      1. Ideologi kiri : komunis, sosialis. Konsep dasarnya adalah kesetaraan.
+      2. Ideologi kanan : taat kepada aturan harus sesuai dengan aturan maka lahirnya kapitalisme
+      
+      Moderat dan Kritis
+      Sikap yang salah 	: fanatik bata (membela sesuatu secara berlebihan) tidak mamapu menganalisis sesuatu.
+      Sikap yang benar	: Mampu berpikir objektif dan berpikir dari dua sisi
+      Moderat	: Bagaimana seseorang berpendapat lebih luas, pemikiran luas dan kedepan
+      Kritis		: Sebelum bertindak harus selalu dipikirkan terlebih dahulu
+      
+      Teknologi
+      Positif			: Waktu
+      Negatif			: Hoax
+      Peran Mahasiswa	: Kita sebagai mahasiswa Informatika minimal harus bisa mengedukasi kepada keluarga.
         #
       `
     },
@@ -81,7 +99,50 @@ Proses memahami kondisi masyarakat secara menyeluruh untuk merancang dan melaksa
       pemateri: "A Defri Crisna Pramudi", 
       tanggal: "Sabtu, 25 April 2026", 
       iconType: "ExternalLink",
-      isi: `
+      isi: ` 
+      Manajemen Aksi
+      Aksi (demonstrasi) suatu model pernyataan sikap penyuaraan pendapat opini atau tuntunan yang dilakukan dengan jumlah massa tertentu dengan teknik tertentu agar mendapat perhatian dari pihak yang dituju tanpa birokrasi.
+      
+      Tujuan 
+        Menekan pembuat keputusan untuk melaksanakan sesuatu.
+      
+      Latar Belakang
+        Jalur penyampaian aspirasi atau bentuknya metode dialog aspirasi rakyat diwakili oleh anggota legislatif.
+      
+      Landasa Hukum
+        UU No. 9/ 1998 tentang mekanisme penyampaian pendapat dimuka umum.
+      
+      Mekanisme Keputusan Aksi
+      Keputusan aksi dianalisis menggunakan SWOT
+      Diskusi awal
+      Pembentukan tim
+      Diskusi lanjutan
+      Aksi lapangan
+      
+      Perangkat aksi :
+      Koordinator lapangan 
+      Orator
+      Negosiator
+      Agitator
+      Logistik
+      Tim kreatif
+      Tim humas
+      Tim medis
+      Dokumenter tim
+      
+      Merancang Aksi : 
+      Planing perangkat.
+      Pelaksanaan
+      Kegiatan paska-aksi
+      
+      Hal yang harus diperhatikan :
+      Tema / grand issue, tema isu yang dipilih 
+      Susun target
+      Skenario
+      Pemberitahuan tergantung pada kebutuhan
+      
+      Fungsi utama web aksi : pusat konsolidasi data, penyebaran manifesto, loket pelapor produk.
+      Kredibilitas dan Otoritas : informasi di website resmi penggerakan lebih sulit dibandingkan di media sosial.      
         #
       `
     },
@@ -92,7 +153,50 @@ Proses memahami kondisi masyarakat secara menyeluruh untuk merancang dan melaksa
       pemateri: "A Ikmal", 
       tanggal: "Sabtu, 02 Mei 2026", 
       iconType: "ExternalLink",
-      isi: `
+      isi: ` 
+      Definisi Otoritas Informasi
+      Kemampuan pemimpin dalam mengelola, mendistribusikan, merahasiakan data untuk menjaga stabilitas dan kedaulatan organisasi.
+      
+      Tiga Pilar Utama
+      1. Struktur, membangun hieraki informasi.
+      2. Validasi, membangu alur verifikasi data.
+      3. Hak Akses, penerapan RBAC (Role Based Access Control)
+      
+      Informatika Arsitektur Hukum 
+      Digital Sovereignty, Perancangan sistem saat ini Code is law.
+      Otoritas Tertinggi, Penguasaan sistem informasi adalah bentuk otoritas
+      Kritis Sistemik 2026
+        58Jt data pendidikan jatuh ke tangan sindikat jahat.
+      
+      Kasus IGRS Komdigi (April 2026)
+        Insiden dan kronologi (celah keamanan pada sistem IGRS membocorkan aset rahasia, kebocoran footage game rahasia yang belum dirilis.
+      
+      Klasifikasi Otoritas Akses
+      
+      Tingkat Akses
+      Publik
+      Internal
+      Rahasia
+
+      Kategori
+      Profile, agenda, berita umum
+      Drop keputusan
+      Data kaderisasi, Strategi
+
+      Wewenang
+      Transparansi dan Brandir
+      Manajerial dan Taktis
+      Proteksi
+
+      Kordasi Kompleksitas Resiko
+      Semakin besar otoritas informasi, semakin tinggi resiko yang harus diintegrasi oleh seorang pemimpin.
+      
+      Siklus Manajemen Otoritas
+      1. Akuisi, Pengumpulan data mentah
+      2. Klasifikasi, Penentuan tingkat kerahasiaan 
+      3. Distribusi, Penyaluran informasi ke pemegang wewenang
+      Proteksi, Penjagaan integritas dan penghapusan data
+      
         #
       `
     },
@@ -102,6 +206,40 @@ Proses memahami kondisi masyarakat secara menyeluruh untuk merancang dan melaksa
       tanggal: "Sabtu, 02 Mei 2026", 
       iconType: "ExternalLink",
       isi: `
+      Pengertian Retorika
+      Retorika adalah studi dan seni menggunakan bahasa (lisan maupun tulisan) secara strategis untuk menyampaikan informasi, memberikan pengaruh, atau memotivasi pendengar dalam situasi tertentu.
+      
+      Tujuan Retorika
+      Setiap kali kita membuka mulut untuk berpidato atau berdiskusi, biasanya ada empat tujuan utama:
+      1. To Inform (Menginformasikan): Memberikan pemahaman baru atau penjelasan teknis kepada audiens.
+      2. To Persuade (Membujuk): Mengubah opini, keyakinan, atau perilaku seseorang.
+      3. To Entertain (Menghibur): Memberikan kesenangan atau relaksasi
+      4. To Actuate (Menggerakkan): Memotivasi audiens untuk segera melakukan tindakan nyata
+      
+      Prinsip dasar Retorika
+      fondasi paling terkenal dalam retorika. Aristoteles merumuskan tiga pilar utama agar sebuah pesan dianggap kredibel dan persuasif:
+      1. Ethos (Kredibilitas): Membangun kepercayaan audiens melalui karakter, keahlian, dan reputasi pembicara.
+      2. Pathos (Emosi): Menyentuh perasaan audiens (sedih, marah, antusias, atau empati) untuk menggerakkan mereka.
+      3. Logos (Logika): Menggunakan data, fakta, argumen yang masuk akal, dan bukti nyata untuk mendukung pesan. 
+      
+      prinsip dasar yang harus dipegang:
+      1. Prinsip Kejelasan (Clarity): Gunakan bahasa yang mudah dipahami, tidak berbelit-belit.
+      2. Prinsip Kebenaran (Veracity): Pesan harus memiliki landasan fakta agar kredibilitas terjaga.
+      3. Prinsip Etika: Tidak memanipulasi data atau menyerang pribadi lawan bicara secara tidak adil.
+      4. Prinsip Kesesuaian (Appropriateness): Menyesuaikan gaya bicara dengan situasi (formal vs informal).
+      Ruang Lingkup
+      
+      Teknik Suara
+      1. StoryTelling
+      2. Bahasa Tubuh
+      3. Struktur yang jelas (dari Penyampaian, pembukaan, isi dan penutup)
+      4. Data dan contoh konkret
+      Latihan
+      Memahami Audiens & cara bicara
+      Background audiens(kayak sd, smp, sma, kuliah dll)
+      Penyesuaian gaya bicara (formal atau non formal)
+      Gunakan Istilah relevan dengan audiens.
+      
         #
       `
     },
